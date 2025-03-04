4d74274cd40b8bc81d69736c583166b74a57ce82
amanar3ra <amanarora3dec@gmail.com>
2025-03-04 09:25:59 -0400

    updated .env

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,6 +4,7 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 // import { fetchCardData } from '@/app/lib/data';
+//
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
